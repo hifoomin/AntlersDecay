03fe22fa91eb50701f949f17137f8c82e50a769b
hifoomin <jakubrataj@protonmail.com>
2023-12-26 15:51:26 +0100

    1.1.0

--- AntlersDecay/Main.cs
+++ AntlersDecay/Main.cs
@@ -3,6 +3,7 @@ using UnityEngine.Rendering.PostProcessing;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using BepInEx.Logging;
+using RoR2;
 
 namespace AntlersDecay
 {
@@ -13,21 +14,24 @@ namespace AntlersDecay
 
         public const string PluginAuthor = "HIFU";
         public const string PluginName = "AntlersDecay";
-        public const string PluginVersion = "1.0.1";
+        public const string PluginVersion = "1.1.0";
 
         public static ManualLogSource adLogger;
 
         public static GameObject ppHolder;
+        public static GameObject ppHolderUI;
 
         public static Color32 rallypointDeltaColor = new(187, 236, 255, 255);
         public static Color32 titanicPlainsColor = new(238, 238, 238, 255);
-        public static Color32 commencementStartColor = new(70, 83, 99, 146);
+        public static Color32 commencementStartColor = new(70, 83, 120, 255);
         public static Color32 commencementMidColor = new(37, 43, 52, 125);
+        public static Color32 grayColor = new(174, 174, 174, 255);
 
         public void Awake()
         {
             adLogger = base.Logger;
             ppHolder ??= InitPP("AntlersDecay");
+            // ppHolderUI ??= InitPP("AntlersDecayUI");
             On.RoR2.Highlight.Awake += Highlight_Awake;
             SceneManager.sceneLoaded += SceneManager_sceneLoaded;
         }
@@ -46,64 +50,127 @@ namespace AntlersDecay
 
         public static void SwitchPP(string sceneName)
         {
-            var colorGrading = ppHolder.GetComponent<PostProcessVolume>().profile.GetSetting<ColorGrading>();
+            var profile = ppHolder.GetComponent<PostProcessVolume>().profile;
+            profile.name = sceneName + " | Antler's Decay";
+            var colorGrading = profile.GetSetting<ColorGrading>();
             adLogger.LogDebug("color grading is " + colorGrading);
             switch (sceneName)
             {
                 case "dampcavesimple":
-                    colorGrading.saturation.value = -32f;
+                    colorGrading.saturation.value = -64f;
                     colorGrading.contrast.value = 20f;
                     colorGrading.colorFilter.value = Color.white;
-                    colorGrading.tint.value = 10f;
                     break;
 
                 case "wispgraveyard":
-                    colorGrading.saturation.value = -20f;
-                    colorGrading.contrast.value = 15f;
+                    colorGrading.saturation.value = -30f;
+                    colorGrading.contrast.value = 5f;
                     colorGrading.colorFilter.value = Color.white;
-                    colorGrading.tint.value = 10f;
                     break;
 
                 case "rootjungle":
-                    colorGrading.saturation.value = -10f;
-                    colorGrading.contrast.value = 20f;
+                    colorGrading.saturation.value = -40f;
+                    colorGrading.contrast.value = 0f;
                     colorGrading.colorFilter.value = Color.white;
-                    colorGrading.tint.value = 10f;
                     break;
 
                 case "frozenwall":
                     colorGrading.saturation.value = 0f;
                     colorGrading.contrast.value = 15f;
                     colorGrading.colorFilter.value = rallypointDeltaColor;
-                    colorGrading.tint.value = 5f;
                     break;
 
                 case "golemplains":
-                    colorGrading.contrast.value = 50f;
-                    colorGrading.saturation.value = -15f;
+                    colorGrading.contrast.value = 25f;
+                    colorGrading.saturation.value = -30f;
                     colorGrading.colorFilter.value = titanicPlainsColor;
                     break;
 
                 case "golemplains2":
-                    colorGrading.contrast.value = 50f;
-                    colorGrading.saturation.value = -15f;
+                    colorGrading.contrast.value = 25f;
+                    colorGrading.saturation.value = -30f;
                     colorGrading.colorFilter.value = titanicPlainsColor;
                     break;
 
                 case "moon2":
-                    var bruh = GameObject.Find("HOLDER: Gameplay Space").transform.GetChild(0).Find("Quadrant 4: Starting Temple").GetChild(0).GetChild(0).Find("FX").GetChild(0).GetComponent<PostProcessVolume>();
+                    var pp = GameObject.Find("HOLDER: Gameplay Space").transform.GetChild(0).Find("Quadrant 4: Starting Temple").GetChild(0).GetChild(0).Find("FX").GetChild(0);
+                    var bruh = pp.GetComponent<PostProcessVolume>();
                     var rampFog = bruh.profile.GetSetting<RampFog>();
                     rampFog.fogColorStart.value = commencementStartColor;
                     rampFog.fogColorMid.value = commencementMidColor;
                     bruh.weight = 0.68f;
+                    var enbyent = pp.GetComponent<HookLightingIntoPostProcessVolume>();
+                    enbyent.overrideAmbientColor = new Color32(71, 71, 81, 255);
+                    enbyent.overrideDirectionalColor = new Color32(41, 34, 41, 255);
+                    colorGrading.saturation.value = -10f;
+                    colorGrading.contrast.value = 15f;
+                    colorGrading.colorFilter.value = Color.white;
+                    break;
 
+                case "arena":
+                    colorGrading.saturation.value = -30f;
+                    colorGrading.contrast.value = 15f;
+                    colorGrading.colorFilter.value = grayColor;
+                    break;
+
+                case "goolake":
+                    colorGrading.contrast.value = 15f;
+                    colorGrading.saturation.value = -20f;
+                    colorGrading.colorFilter.value = titanicPlainsColor;
+                    break;
+
+                case "voidraid":
+                    colorGrading.saturation.value = -30f;
+                    colorGrading.contrast.value = 15f;
+                    colorGrading.colorFilter.value = Color.white;
+                    break;
+
+                case "voidstage":
+                    colorGrading.saturation.value = -20f;
+                    colorGrading.contrast.value = 15f;
+                    colorGrading.colorFilter.value = Color.white;
+                    break;
+
+                case "skymeadow":
+                    colorGrading.saturation.value = -20f;
+                    colorGrading.contrast.value = 5f;
+                    colorGrading.colorFilter.value = Color.white;
+                    break;
+
+                case "foggyswamp":
+                    colorGrading.contrast.value = 5f;
+                    colorGrading.saturation.value = -30f;
+                    colorGrading.colorFilter.value = titanicPlainsColor;
+                    break;
+
+                case "blackbeach":
+                    colorGrading.saturation.value = -30f;
+                    colorGrading.contrast.value = 10f;
+                    colorGrading.colorFilter.value = Color.white;
+                    break;
+
+                case "blackbeach2":
+                    colorGrading.saturation.value = -30f;
+                    colorGrading.contrast.value = 10f;
+                    colorGrading.colorFilter.value = Color.white;
+                    break;
+
+                case "sulfurpools":
+                    colorGrading.saturation.value = -30f;
+                    colorGrading.contrast.value = 5f;
+                    colorGrading.colorFilter.value = Color.white;
+                    break;
+
+                case "shipgraveyard":
+                    colorGrading.saturation.value = -20f;
+                    colorGrading.contrast.value = 10f;
+                    colorGrading.colorFilter.value = Color.white;
                     break;
 
                 default:
-                    colorGrading.saturation.value = -10f;
-                    colorGrading.contrast.value = 30f;
+                    colorGrading.saturation.value = -15f;
+                    colorGrading.contrast.value = 15f;
                     colorGrading.colorFilter.value = Color.white;
-                    colorGrading.tint.value = 5f;
                     break;
             }
         }
@@ -128,9 +195,8 @@ namespace AntlersDecay
             pp.sharedProfile = ppProfile;
 
             colorGrading.enabled.value = true;
-            colorGrading.saturation.value = -10f;
+            colorGrading.saturation.value = -20f;
             colorGrading.contrast.value = 30f;
-            colorGrading.tint.value = 10f;
             colorGrading.tonemapper.value = Tonemapper.Custom;
             colorGrading.toneCurveToeStrength.value = 1f;
             colorGrading.toneCurveToeLength.value = 0.15f;
@@ -140,7 +206,7 @@ namespace AntlersDecay
             var grain = ppProfile.AddSettings<Grain>();
             grain.SetAllOverridesTo(true);
 
-            grain.intensity.value = 0.1f;
+            grain.intensity.value = 0.13f;
             var bloom = ppProfile.AddSettings<Bloom>();
             bloom.SetAllOverridesTo(true);
 
